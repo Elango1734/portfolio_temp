d511402e3a77a1e73943e9999d2917200cadb4a0
Dharineesh235 <dharineesh235@gmail.com>
2025-01-05 17:56:37 +0530

    profile page stage-2 commit (without background)

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -10,26 +10,26 @@ const inter = Inter({
 
 const Profile = () => {
   return (
-    <div style={{}} className={`${inter.variable} font-sans lg:flex md:flex pt-20`}>
+    <div style={{}} className={`font-sans flex flex-col justify-center items-center lg:flex-row md:flex-row gap-10 pt-10`}>
         {/* left compartment */}
-      <div className="flex text-white flex-col justify-start items-center w-2/4 gap-3">
-        <div className='self-start text-4xl flex w-full'>
+      <div className="flex text-white flex-col justify-center items-center gap-5">
+        <div className='text-2xl lg:text-3xl md:3xl flex w-full justify-center lg:justify-start md:justify-start'>
           <h1>I'm </h1>
         </div>
-        <div className='self-start text-7xl font-bold flex w-full border-2 border-solid border-sky-800'>
+        <div className=' md:text-5xl lg:text-7xl text-4xl font-bold flex w-full justify-center lg:justify-start md:justify-start'>
             <h1>Dharineesh </h1><h2 className='text-sky-800'>&nbsp;G&nbsp;G</h2>
         </div>
-        <div className='self-start text-2xl font-medium flex '>
-            <div>a</div> <div className=''>&nbsp;Frontend Developer</div>
+        <div className='text-2xl font-medium flex justify-center lg:justify-start md:justify-start w-full'>
+           <div className=''>a Frontend Developer</div>
         </div>
-        <div className='self-start flex gap-3'>
+        <div className='flex gap-3 justify-center lg:justify-start md:justify-start w-full'>
             <FaLinkedin/>
             <FaGithub/>
             <SiGmail/>
         </div>
       </div>
       {/* right compartment */}
-      <div className="flex text-white justify-center items-center w-1/2 gap-3">
+      <div className="flex text-white justify-center items-center flex-1 gap-3">
             <div className='rounded-full bg-sky-800 py-3 px-5'>Hire ME</div>
             <div className='border-solid rounded-full p-3 border-2 border-sky-800'>My Works</div>
         </div>
