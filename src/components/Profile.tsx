e30a3c9347fc8635335ad84318509a8a5b8501f0
Dharineesh235 <dharineesh235@gmail.com>
2025-01-05 13:00:50 +0530

    profile page stage-2 commit (without background)

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,23 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { Inter } from 'next/font/google';
 import { FaGithub, FaLinkedin } from 'react-icons/fa'
-import { SiGmail } from 'react-icons/si'
+import { SiGmail } from 'react-icons/si';
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+})
 
 const Profile = () => {
   return (
-    <div style={{}} className='lg:flex md:flex pt-10'>
+    <div style={{}} className={`${inter.variable} font-sans lg:flex md:flex pt-20`}>
         {/* left compartment */}
       <div className="flex text-white flex-col justify-start items-center w-2/4 gap-3">
-        <div className='self-start text-3xl font-bold flex'>
-            <h1 className=''>DHARINEESH </h1><h2 className='text-sky-800'>&nbsp;G&nbsp;G</h2>
+        <div className='self-start text-4xl flex w-full'>
+          <h1>I'm </h1>
+        </div>
+        <div className='self-start text-7xl font-bold flex w-full border-2 border-solid border-sky-800'>
+            <h1>Dharineesh </h1><h2 className='text-sky-800'>&nbsp;G&nbsp;G</h2>
         </div>
         <div className='self-start text-2xl font-medium flex '>
-            <div>I'm a</div> <div className='text-sky-800'>&nbsp;Frontend Developer</div>
-        </div>
-        <div className='self-start'>
-            I'm professional web developer with strong skill in HTML, 
-            CSS, JavaScript, Tailwind, JQuery etc. I have working in this 
-            field almost 3 years and all projects are complited successfully 
-            with 100% client satisfiction.
+            <div>a</div> <div className=''>&nbsp;Frontend Developer</div>
         </div>
         <div className='self-start flex gap-3'>
             <FaLinkedin/>
@@ -26,8 +29,9 @@ const Profile = () => {
         </div>
       </div>
       {/* right compartment */}
-      <div className="flex text-white flex-col justify-center items-center w-1/2">
-            <h4 className='h-20 w-20 rounded-full bg-sky-800 text-center'>GG</h4>
+      <div className="flex text-white justify-center items-center w-1/2 gap-3">
+            <div className='rounded-full bg-sky-800 py-3 px-5'>Hire ME</div>
+            <div className='border-solid rounded-full p-3 border-2 border-sky-800'>My Works</div>
         </div>
     </div>
   )
