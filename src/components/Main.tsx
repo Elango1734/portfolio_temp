e7c488263141f3bef327cc121799189250c6434e
Dharineesh235 <dharineesh235@gmail.com>
2025-01-09 21:30:32 +0530

    timeout for loader(spinner) added

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,15 +2,35 @@
 import React, { ReactNode, useEffect, useRef, useState } from 'react'
 import CrossSpinner from './CrossSpinner';
 import Header from './Header';
+const TIMEOUT:number = 1000;
 
 type children = { children: ReactNode; }
 
 const Main: React.FC<children> = ({ children }) => {
   const [loading, setLoading] = useState<boolean>(true);
+  const [visible, setVisible] = useState<boolean>(false);
   const parallaxRef = useRef<HTMLDivElement>(null);
   const imageUrl: string = '/images/mountain.jpg';
 
-  useEffect(() => { const img = new Image(); img.src = imageUrl; img.onload = () => setLoading(false); }, [imageUrl]);
+  useEffect(() => {
+    const img = new Image();
+    img.src = imageUrl;
+
+    let loaderTimer:NodeJS.Timeout;
+    let fadeTimer:NodeJS.Timeout;
+    img.onload = () => {
+      loaderTimer = setTimeout(()=>{
+        setLoading(false);
+      },TIMEOUT);
+      fadeTimer = setTimeout(()=>{
+        setVisible(true);
+      },TIMEOUT+50)
+    };
+    return ()=>{
+      clearTimeout(loaderTimer);
+      clearTimeout(fadeTimer);
+    }
+  }, [imageUrl]);
 
   useEffect(() => {
 
@@ -32,10 +52,10 @@ const Main: React.FC<children> = ({ children }) => {
     };
 
   }, []);
-
+  console.log(loading);
   return (
     <div className=''>
-      {(loading && !children) ?
+      {(loading) ?
         <CrossSpinner />
         :
         <>
@@ -50,7 +70,7 @@ const Main: React.FC<children> = ({ children }) => {
               backgroundPosition: 'center 20%',
               transition: 'all 0.1s linear'
             }}
-            className='min-w-80 max-w-screen-2xl bg-cover bg-center w-full'
+            className={`min-w-80 max-w-screen-2xl bg-cover bg-center w-full fade-in ${visible?'visible':''} `}
           >
             {children}
           </div>
